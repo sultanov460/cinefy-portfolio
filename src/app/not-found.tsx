cd26d67bc4b5a48ff5c837af5318a41bc017bc54
Abbas Ibragimov <abbasibragimov123@gmail.com>
2025-10-29 20:11:28 +0400

    Update not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,16 +1,14 @@
-import React from "react";
-
-const notfound = () => {
+const NotFound = () => {
   return (
-    <div className="flex flex-col gap-2 text-center mt-15 items-center ">
-      <h1 className="font-extrabold text-7xl md:text-9xl text-primary tracking-[5px] border-b border-primary">
+    <div className="flex flex-col gap-2 text-center justify-center h-full items-center">
+      <h1 className="relative font-extrabold text-7xl pb-3 md:text-9xl text-primary tracking-[5px] titleLine">
         404
       </h1>
-      <p className="font-medium text-sm md:text-2xl tracking-[3px]">
+      <p className="font-medium text-sm md:text-3xl tracking-[3px] uppercase">
         Page not found.
       </p>
     </div>
   );
 };
 
-export default notfound;
+export default NotFound;
