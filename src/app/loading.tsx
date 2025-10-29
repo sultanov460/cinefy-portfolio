4a9940299603e1ab5ae047cbdee5e430fe4fd8c6
Abbas Ibragimov <abbasibragimov123@gmail.com>
2025-10-29 20:16:08 +0400

    Update

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,7 +3,7 @@ import { ScaleLoader } from "react-spinners";
 
 const Loading = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="fixed inset-0 bg-black/70 flex items-center justify-center">
       <ScaleLoader color="#f6be00" height={50} width={5} />
     </div>
   );
