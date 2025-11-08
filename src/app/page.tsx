ef58500ede3570efbfd03dc58f62ec2b484a2ad3
Rafiq Sultanov <sultanovrafiq71@gmail.com>
2025-11-08 18:16:41 +0400

    Tasks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,15 @@ import { MovieSection } from "@/widgets/MovieSection";
 const Home = async () => {
   const { results: trendingMovies } = await getTrendingMovies();
   const { results: trendingSeries } = await getTrendingSeries();
+  // TODO:
+  // 1:WRIGHT FULL SEO
+  // 2:Create a details page for actors (Dz)
+  // 3:Display trailers for movies/series in their details page
+  // 4:Create a details page for series (Dz)
+  // 5:Create a mobile version for the whole website (Dz)
+  // 6:Implement searching for movies/series
+  // 7:Create a third movie section in the home page
+  // 8:Create a favicon for the website (generate in Canva) (Dz)
 
   return (
     <>
